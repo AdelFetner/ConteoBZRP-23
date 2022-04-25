877f103a064ed2e1b584f81fc05880f0e78217f1
Adel Fetner <adelensoc@gmail.com>
2022-04-25 16:52:50 -0300

    Arreglado problema minutos

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let conteo = setInterval(() =>{
     // calcular tiempo
     let horas = Math.floor((faltaTiempo % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
     let minutos = Math.floor((faltaTiempo % (1000 * 60 * 60)) / (1000 * 60))
+    minutos = minutos.toString().padStart(2, "0")
     let segundos = Math.floor((faltaTiempo % (1000 * 60)) / 1000)
     segundos = segundos.toString().padStart(2, "0")
 
